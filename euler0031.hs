9aea3416d4287e1b212605535d79d3bcc7795df3
Michael Schubart <michael@schubart.net>
2009-04-19 02:46:58 +0100

    Renamed function.

--- euler0031.hs
+++ euler0031.hs
@@ -1,6 +1,6 @@
-numSols :: Int -> [Int] -> Int
-numSols 0 _  = 1 -- Target 0p: One solution, available coins don't matter.
-numSols _ [] = 0 -- Some target but no coins left: No solution.
-numSols target (c:cs) = sum [ combos (target - x) cs | x <- [0,c..target] ]
+ways :: Int -> [Int] -> Int
+ways 0 _  = 1 -- Target 0p: One solution, available coins don't matter.
+ways _ [] = 0 -- Some target but no coins left: No solution.
+ways target (c:cs) = sum [ ways (target - x) cs | x <- [0,c..target] ]
 
-main = print $ combos 200 [200, 100, 50, 20, 10, 5, 2, 1]
+main = print $ ways 200 [200, 100, 50, 20, 10, 5, 2, 1]
