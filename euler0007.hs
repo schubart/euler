d3742a86ba48a7e43ac76bf2717abc4f6ebecea5
Michael Schubart <michael@schubart.net>
2009-05-03 14:32:47 +0100

    Solved problem 7.

---
+++ euler0007.hs
@@ -0,0 +1,9 @@
+primes = 2 : filter ((==1) . length . primeFactors) [3,5..]
+
+primeFactors n = factors n primes
+    where factors n (p:ps)
+              | p * p > n      = [n]
+              | n `mod` p == 0 = p : factors (n `div` p) (p:ps)
+              | otherwise      = factors n ps
+
+main = print $ primes !! 1000
