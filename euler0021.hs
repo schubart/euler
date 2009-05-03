1f0ba35e0465fb8d8cbfbb14f9cd923d47d7c29b
Michael Schubart <michael@schubart.net>
2009-05-03 16:23:33 +0100

    Improved speed of problem 21.

--- euler0021.hs
+++ euler0021.hs
@@ -1,8 +1,20 @@
--- TODO: Slow. (~ 35 sec)
+import Data.List
+
+-- Calculation of divisors based on achiles' code (10/15/2007), but fixes
+-- case of perfect squares (why does his code provide the right solution
+-- in spit of this issue?)
+
+-- When we find one divisor, we automatically find a second divisor.
+-- Example: 3 divides 12, 12 div 3 is 4 -> 4 divides 12.
+-- Exception: Perfect square numbers.
+-- Example: 3 divides 9, but only consider 3 once (using nub.)
+divs n = 1 : concat [ nub [x, n `div` x] | x <- [2..root], n `mod` x == 0 ]
+    where
+      root = (round . sqrt . encodeFloat n) 0
 
 main = print $ sum $ filter isAmicable [1..9999]
     where
       isAmicable n = (n /= (d n)) && (n == (d (d n)))
       -- d: As in problem defintion.
-      d = sum . properDivisors
-      properDivisors n = 1 : filter ((==0) . (n `mod`)) [2..(n `div` 2)]
+      d = sum . divs
+
