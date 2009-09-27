7732a4f748b92e1aeb12d9da56ed4ee3e00dffdb
Michael Schubart <michael@schubart.net>
2009-09-27 09:46:20 +0100

    Solved problem 112.

---
+++ euler0112.hs
@@ -0,0 +1,14 @@
+import Data.List
+
+isIncreasing x = and $ zipWith (<=) x (tail x)
+isDecreasing x = and $ zipWith (>=) x (tail x)
+isBouncy x = not (isIncreasing x) && not (isDecreasing x)
+
+density :: [Bool] -> [(Int, Int)]
+density xs = scanl aggregate (0, 0) xs
+    where aggregate (a, b) t = if t
+                               then (a + 1, b + 1)
+                               else (a,     b + 1)
+
+# TODO prettier, use Ratio type?
+main = print $ find (\(a, b) -> a * 100 >= b * 99) $ tail $ density $ map (isBouncy . show) [1..]
